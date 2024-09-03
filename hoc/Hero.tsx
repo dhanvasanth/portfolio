f57cf6535a932f6504b4025c8f22b1bd37c6ae6c
dhanvasanth <dhanvasanthr@gmmail.com>
2024-09-03 09:50:59 +0530

    abi

--- hoc/Hero.tsx
+++ hoc/Hero.tsx
@@ -1,6 +1,6 @@
-import { Spotlight } from "./ui/Spotlight"
-import { TextGenerateEffect } from "./ui/TextGenerateEffect"
-import MagicButton from "./MagicButton"
+import { Spotlight } from "../components/ui/Spotlight"
+import { TextGenerateEffect } from "../components/ui/TextGenerateEffect"
+import MagicButton from "../components/MagicButton"
 import { FaLocationArrow } from "react-icons/fa6";
 const Hero = () => {
   return (
